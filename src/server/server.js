b02b85317af2c8179e87e91e52c859b76e6b840e
Hans Pelle Jart <hanspellejart@gmail.com>
2021-06-21 20:45:40 +0200

    scripts missing...

--- src/server/server.js
+++ src/server/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const port = 3000;
 const server = app.listen(port, () => {
     console.log('server running');
-    console.log(`kører på localhost: ${port}`);
+    console.log(`kører på lokalvært: ${port}`);
 });
